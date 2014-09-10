4cc2b5f377385b7663eb6db36a87d7d8d8231253
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2014-09-10 11:44:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python
+
+"""[License: GPL3]
+ 
+ This file is part of FuMa.
+ 
+ flaimapper is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ flaimapper is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+ Documentation as defined by:
+ <http://epydoc.sourceforge.net/manual-fields.html#fields-synonyms>
+"""
+
+import flaimapper
+
+from distutils.core import setup
+
+setup(name='fuma',
+      version=flaimapper.__version__,
+      description='Fusion Matcher',
+      author=flaimapper.__author__,
+      url='https://github.com/yhoogstrate/fuma',
+      scripts=["bin/fuma"],
+      packages=['fuma'],
+     )
