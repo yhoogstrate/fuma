0281ed2afdd1bad12de84f39e41757147af93a7e
yhoogstrate <y.hoogstrate@erasmusmc.nl>
2014-09-10 11:47:10 +0200

    Create __init__.py

---
+++ fuma/__init__.py
@@ -0,0 +1,6 @@
+
+__version_info__ = ('1', '0', '0', 'alpha')
+__version__ = '.'.join(__version_info__)
+__author__ = 'Youri Hoogstrate'
+__homepage__ = 'https://github.com/yhoogstrate/fuma'
+__license__ = 'The GNU General Public License v3.0 (GPL3) - http://www.gnu.org/copyleft/gpl.html'
