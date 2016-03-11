d279a7709a665a1f0b5445c94cd9b9fbeecd74b7
youri <y.hoogstrate@erasmusmc.nl>
2016-03-11 21:42:21 +0100

    Version++

--- fuma/__init__.py
+++ fuma/__init__.py
@@ -21,7 +21,7 @@
  <http://epydoc.sourceforge.net/manual-fields.html#fields-synonyms>
 """
 
-__version_info__ = ('2', '11', '5')
+__version_info__ = ('2', '11', '6')
 __version__ = '.'.join(__version_info__) if (len(__version_info__) == 3) else '.'.join(__version_info__[0:3])+"-"+__version_info__[3]
 __author__ = 'Youri Hoogstrate'
 __homepage__ = 'https://github.com/yhoogstrate/fuma'
