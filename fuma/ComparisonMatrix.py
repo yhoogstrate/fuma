5c9a8a91eee69522925ff0754ed38aae94dde08a
y.hoogstrate@erasmusmc.nl <y.hoogstrate@erasmusmc.nl>
2016-03-24 16:46:39 +0100

    Improved iterator

--- fuma/ComparisonMatrix.py
+++ fuma/ComparisonMatrix.py
@@ -59,13 +59,18 @@ class ComparisonMatrix:
 		
 		"""
 		iter1: 
-		0,0 | 0,1 0,2 | 0,3 0,4 0,5 | 0,6 0,7
+		
+		0,7 | 1,7 2,7 | 3,7 4,7 5,7 | 6,7 7,7
+		
+		iter2:
+		0,6 | 1,6 2,6 | 3,6 4,6 5,6 | 6,6
+		
+		
 		"""
-		for i in range(len(fusions)):
-			fusion_i = fusions[i]
-			for j in range(len(fusions)):
-				if i != j:
-					print i,j
+		for y in range(len(fusions)-1,0,-1):
+			for x in range(y+1):
+				print str(x)+","+str(y)+" ",
+			print
 	
 	def __len__(self):
 		return len(self.experiments)
